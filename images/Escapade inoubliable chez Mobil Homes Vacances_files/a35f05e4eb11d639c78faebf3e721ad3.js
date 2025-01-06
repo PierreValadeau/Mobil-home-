09d4dd824a5098268a88c5684debcbdb8b2c831b
Pierre Valadeau <p.valadeau@outlook.fr>
2025-01-06 09:46:12 +0100

    fixing css to background img

---
+++ images/Escapade inoubliable chez Mobil Homes Vacances_files/a35f05e4eb11d639c78faebf3e721ad3.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(){jQuery.ajax({url:ajaxurl,type:'post',data:{action:'get_the_page',ctc_referer:document.referrer,ctc_uri:location.pathname,ctc_title:document.getElementsByTagName("title")[0].innerHTML,}})})
+;
