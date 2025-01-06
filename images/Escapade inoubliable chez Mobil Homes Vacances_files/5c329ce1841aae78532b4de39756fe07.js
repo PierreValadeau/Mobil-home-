d13ff10551d8e3b6b8f6e3920c98fbdf84532a8a
Pierre Valadeau <p.valadeau@outlook.fr>
2025-01-06 10:03:59 +0100

    adding background img to hero section

--- images/Escapade inoubliable chez Mobil Homes Vacances_files/5c329ce1841aae78532b4de39756fe07.js
+++
@@ -1,3 +0,0 @@
-'use strict';document.addEventListener('wpcf7statuschanged',function(event){const form=event.target,button=form.querySelector('.wpcf7-submit');if(event?.detail?.status==='submitting'){button.setAttribute('disabled','disabled')}
-if(event?.detail?.status!=='submitting'){button.removeAttribute('disabled')}},!1)
-;
--- images/Escapade inoubliable chez Mobil Homes Vacances_files/a35f05e4eb11d639c78faebf3e721ad3.js
+++
@@ -1,2 +0,0 @@
-jQuery(document).ready(function(){jQuery.ajax({url:ajaxurl,type:'post',data:{action:'get_the_page',ctc_referer:document.referrer,ctc_uri:location.pathname,ctc_title:document.getElementsByTagName("title")[0].innerHTML,}})})
-;
