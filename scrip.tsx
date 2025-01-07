49b9e3f46d20194a0def752e9a4211a01dc9afd6
Pierre Valadeau <p.valadeau@outlook.fr>
2025-01-07 10:47:05 +0100

    create animation on page

--- scrip.tsx
+++ scrip.tsx
@@ -1,24 +1,20 @@
-// Script js 
-
-// Fonction pour vérifier si un élément est visible 
-
-function isInViewport(element){
+// Fonction pour vérifier si un élément est visible
+function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  return(
-    rect.top >= 0 && 
+  return (
+    rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight ) &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
-
 
 // Fonction pour gérer les animations au scroll
 function handleScroll() {
   const elements = document.querySelectorAll('.animate');
   elements.forEach((el) => {
-    if (isInViewport(el)) {
-      el.classList.add('visible'); // Ajoute la classe pour déclencher l'animation
+    if (isInViewport(el) && !el.classList.contains('visible')) {
+      el.classList.add('visible'); // Ajoute la classe si l'élément devient visible
     }
   });
 }
